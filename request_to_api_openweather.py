8f64ff3dd6c7e011025c776a3c4ffdc8d30d0d36
IWOrLaFI <wolf6961@gmail.com>
2022-01-05 13:48:33 +0200

    request_to_api_openweather.py
    rename static API_KEY -> TOKEN

--- request_to_api_openweather.py
+++ request_to_api_openweather.py
@@ -1,8 +1,8 @@
 import requests
-API_KEY = '8410a325b25bba0213254a8489ade0bd'
+TOKEN = '8410a325b25bba0213254a8489ade0bd'
 
 
 def request_to_api(lat, lon):
     url = f'https://api.openweathermap.org/' \
-          f'data/2.5/onecall?lat={lat}&lon={lon}&appid={API_KEY}&units=metric'
+          f'data/2.5/onecall?lat={lat}&lon={lon}&appid={TOKEN}&units=metric'
     return requests.get(url).json()
