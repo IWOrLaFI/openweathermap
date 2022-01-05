42acaa1174e101dbbcc0a0e971196ab28178eb2c
IWOrLaFI <wolf6961@gmail.com>
2022-01-05 17:06:24 +0200

    update db

--- request_samples.py
+++ request_samples.py
@@ -3,25 +3,25 @@ import requests
 
 list_requests = [
     ['/cities GET',
-        'http://127.0.0.1:5000/cities'],
+     'http://127.0.0.1:5000/cities'],
 
     ['/mean GET  (value_type=temp, city=dnipro)',
-        'http://127.0.0.1:5000/mean?value_type=temp&city=dnipro'],
+     'http://127.0.0.1:5000/mean?value_type=temp&city=dnipro'],
 
     ['/mean GET  (value_type=pressure, city=lviv)',
-        'http://127.0.0.1:5000/mean?value_type=pressure&city=lviv'],
+     'http://127.0.0.1:5000/mean?value_type=pressure&city=lviv'],
 
-    ['/records GET  (city=dnipro, start_dt=28-12-2021, end_dt=01-01-2022)',
-        'http://127.0.0.1:5000/records?city=dnipro&start_dt=28-12-2021&end_dt=01-01-2022'],
+    ['/records GET  (city=dnipro, start_dt=08-01-2022, end_dt=12-01-2022)',
+     'http://127.0.0.1:5000/records?city=dnipro&start_dt=08-01-2022&end_dt=12-01-2022'],
 
-    ['/records GET (city=Oleksandriya, start_dt=30-12-2021, end_dt=31-12-2021)',
-        'http://127.0.0.1:5000/records?city=Oleksandriya&start_dt=30-12-2021&end_dt=31-12-2021'],
+    ['/records GET (city=Oleksandriya, start_dt=10-01-2022, end_dt=11-01-2022)',
+     'http://127.0.0.1:5000/records?city=Oleksandriya&start_dt=10-01-2022&end_dt=11-01-2022'],
 
     ['/moving_mean GET (value_type=pressure, city=lviv)',
-        'http://127.0.0.1:5000/moving_mean?value_type=pressure&city=lviv'],
+     'http://127.0.0.1:5000/moving_mean?value_type=pressure&city=lviv'],
 
     ['/moving_mean GET (value_type=temp, city=dnipro)',
-        'http://127.0.0.1:5000/moving_mean?value_type=temp&city=dnipro']
+     'http://127.0.0.1:5000/moving_mean?value_type=temp&city=dnipro']
 ]
 
 
